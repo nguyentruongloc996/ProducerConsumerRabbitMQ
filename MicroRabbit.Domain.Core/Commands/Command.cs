070d0ed848c5faf0d7f4710f219cff448254594b
Truong Loc <nguyentruongloc996@gmail.com>
2024-11-24 16:26:27 +0700

    Create domain Interfaces and Class

---
+++ MicroRabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,13 @@
+﻿using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; set; }
+        protected Command() 
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
